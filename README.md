c3b3ea13e8b46aade370eee01e4622320d4ca3b6
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2023-10-26 14:10:20 +0200

    Reusable workflow (#16)
    * reusable
    
    * bytt-jsonview
    
    * linting
    
    * redeme
    
    * wip

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # flex-juridisk-vurdering-test-backend
 
-Frontend for jurdisk vurdering test
+Frontend for jurdisk vurdering test. Viser innhold på topicet og validerer mot skjema.
+Fetcher data klientside med CORS.
 
 # Henvendelser
-
 
 Spørsmål knyttet til koden eller prosjektet kan stilles til flex@nav.no
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,13 @@
 import type { NextPage } from 'next'
 
-
 const Home: NextPage = () => {
-  return (
-    <div >
-
-      <main >
-        <h1>
-          Juridisk vurdering test frontend
-        </h1>
-      </main>
-    </div>
-  )
+    return (
+        <div>
+            <main>
+                <h1>Juridisk vurdering test frontend</h1>
+            </main>
+        </div>
+    )
 }
 
 export default Home
